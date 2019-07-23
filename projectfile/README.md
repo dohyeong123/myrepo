9f999fcc5d52985a8ba8a3a0328634aed961c847
sudond <sudond@naver.com>
2019-07-23 17:34:05 +0900

    unit3

---
+++ projectfile/README.md
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+# mymyrepo
+# mymyrepo
+# mymyrepo
+# mymyrepo
+=======
+# myrepo
+>>>>>>> a4fed73c09506294733a74fc8f3fda751f057705
---
+++ projectfile/accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(
+                request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            return redirect('blog')
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = auth.authenticate(request, username = username, password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('blog')
+        else : 
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        redirect('blog')
+    return render(request, 'accounts/login.html')
